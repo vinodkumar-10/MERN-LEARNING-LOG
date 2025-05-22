c492a668a9914a7791e917f49cf1afb32c4264cb
Vinod Kumar <kilarivinodkumar@gmail.com>
2025-05-22 07:46:12 +0530

    Rename Welcome component with correct casing and fix imports

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from './components/welcome';
+
+function App() {
+  return (
+    <div>
+      <h1>🚀 My React App</h1>
+      <Welcome name="Vinod" />
+      <Welcome name="Venky" />
+    </div>
+  );
+}
+export default App;
+
